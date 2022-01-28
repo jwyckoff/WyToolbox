ee521a86fcbb36f485030b1d9a2fe3d1ff530af5
Jason Wyckoff <jason@jasonwyckoff.com>
2022-01-28 08:27:59 -0600

    re-org

---
+++ _Install/install-choco.ps1
@@ -0,0 +1,62 @@
+
+
+echo '-------------------------------------------------------------------------------------------'
+echo '-'
+echo '- Install Chocolatey'
+echo '-'
+echo '-------------------------------------------------------------------------------------------'
+
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+choco install powershell-core -y
+choco install brave -y
+choco install notepadplusplus -y 
+choco install 7zip -y
+choco install git -y
+choco install powertoys -y
+choco install clickie -y
+choco install autohotkey -y
+
+echo '-------------------------------------------------------------------------------------------'
+echo '-'
+echo '- Install Hugo'
+echo '-'
+echo '-------------------------------------------------------------------------------------------'
+choco install hugo-extended -confirm
+
+echo '-------------------------------------------------------------------------------------------'
+echo '-'
+echo '- Install Powershell Core'
+echo '-'
+echo '-------------------------------------------------------------------------------------------'
+choco install powershell-core -y
+
+# choco upgrade powershell-core -y
+
+echo '-------------------------------------------------------------------------------------------'
+echo '-'
+echo '- Install Python'
+echo '-'
+echo '-------------------------------------------------------------------------------------------'
+choco install -y python3
+
+
+echo '-------------------------------------------------------------------------------------------'
+echo '-'
+echo '- Install nodejs'
+echo '-'
+echo '-------------------------------------------------------------------------------------------'
+choco install -y nodejs
+
+
+
+echo '-------------------------------------------------------------------------------------------'
+echo '-'
+echo '- Install pandoc'
+echo '-'
+echo '-------------------------------------------------------------------------------------------'
+choco install pandoc
+winget install -e --id JohnMacFarlane.Pandoc
+
+echo '-------------------------------------------------------------------------------------------'
+choco install docker-desktop
---
+++ _Install/install-winget.ps1
@@ -0,0 +1,19 @@
+
+
+
+winget install PowerShell -s winget
+winget upgrade PowerShell -s winget
+winget install powertoys -s windget
+
+winget install -e --id Microsoft.VisualStudioCode
+winget install -e --id OpenJS.Nodejs
+winget install -e --id Microsoft.AzureCLI
+
+winget install -e --id Microsoft.AzureDataStudio
+winget install -e --id Microsoft.AzureFunctionsCoreTools
+winget install -e --id Microsoft.Bicep
+winget install --id Microsoft.officedeploymenttool
+
+winget install -e --id Microsoft.PowerBI
+winget install -e --id Microsoft.VisualStudio.Enterprise
+winget install -e --id Microsoft.WindowsTerminal
