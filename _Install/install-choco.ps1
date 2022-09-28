89a3e5f4f297ecb3f78f1d0be34ba584000db62c
Jason Wyckoff <jason@jasonwyckoff.com>
2022-09-28 14:12:09 +0000

    Update install-choco.ps1

--- _Install/install-choco.ps1
+++ _Install/install-choco.ps1
@@ -9,13 +9,15 @@ echo '--------------------------------------------------------------------------
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 choco install powershell-core -y
-choco install brave -y
+choco install visualstudiocode -y
 choco install notepadplusplus -y 
 choco install 7zip -y
-choco install git -y
 choco install powertoys -y
+choco install git -y
 choco install clickie -y 
 choco install autohotkey -y
+
+choco install brave -y
 
 echo '-------------------------------------------------------------------------------------------'
 echo '-'
