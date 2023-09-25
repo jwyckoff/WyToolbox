2d2f651f35e70d017c93323055bd3282b8c8b87b
Jason Wyckoff <jason@jasonwyckoff.com>
2023-09-25 06:39:44 -0500

    2023-09-25 06:39:44

---
+++ _Install/git-defaults.ps1
@@ -0,0 +1,7 @@
+$defaultBranchName = "main"
+$defaultUserName = "Jason Wyckoff"
+$defaultUserEmail = "jason@jasonwyckoff.com"
+
+git config --global init.defaultBranch $defaultBranchName
+git config --global user.name $defaultUserName
+git config --global user.email $defaultUserEmail
